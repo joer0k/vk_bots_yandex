ecd2b503ea9446e0392c4599d882092ce9d490ef
joer0k <ryabokon.niki08@gmail.com>
2025-05-08 14:33:22 +0300

    'запрос записи на стене'

---
+++ main.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+
+import vk_api
+from CONFIG import PASSWORD, LOGIN
+
+
+def captcha_handler(captcha):
+    """ При возникновении капчи вызывается эта функция и ей передается объект
+        капчи. Через метод get_url можно получить ссылку на изображение.
+        Через метод try_again можно попытаться отправить запрос с кодом капчи
+    """
+
+    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
+
+    # Пробуем снова отправить запрос с капчей
+    return captcha.try_again(key)
+
+
+def auth_handler():
+    """ При двухфакторной аутентификации вызывается эта функция. """
+
+    # Код двухфакторной аутентификации,
+    # который присылается по смс или уведомлением в мобильное приложение
+    # или код из приложения - генератора кодов
+    key = input("Enter authentication code: ")
+    # Если: True - сохранить, False - не сохранять.
+    remember_device = True
+
+    return key, remember_device
+
+
+def main():
+    login, password = LOGIN, PASSWORD
+    vk_session = vk_api.VkApi(
+        login, password,
+        # функция для обработки двухфакторной аутентификации
+        auth_handler=auth_handler,
+        captcha_handler=captcha_handler
+    )
+
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+    vk = vk_session.get_api()
+    # Используем метод wall.get
+    response = vk.wall.get(count=5, offset=0)
+    if response['items']:
+        for i in response['items']:
+            time = datetime.fromtimestamp(i['date'])
+            print(i['text'])
+            print(f'date: {time.strftime('%Y-%m-%d')}, time: {time.strftime("%H:%M:%S")}')
+            print()
+
+
+if __name__ == '__main__':
+    main()
